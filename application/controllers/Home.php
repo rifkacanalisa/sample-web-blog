17a2cee4548f51c2a348cf02a9716fca2b1c80e5
rifkacanalisa <50352632+rifkacanalisa@users.noreply.github.com>
2021-02-18 08:23:25 +0700

    mencoba perbaiki session login ke 6

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,5 +1,12 @@
 <?php   
 class Home extends CI_Controller {
+        
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('User_model');
+        }
+
 	public function index ()
 	{
         $data['judul'] = "Home";
