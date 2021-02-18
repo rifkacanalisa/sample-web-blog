8276d39aa33a7a43a59565eafb512d479913a8ec
rifkacanalisa <50352632+rifkacanalisa@users.noreply.github.com>
2021-02-18 12:11:56 +0700

    random urutan fandom

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -2,7 +2,10 @@
 
 class Home_model extends CI_Model{
     public function getPicHome(){
-        return $this->db->get('fandom')->result_array();
+        return $this->db
+        ->order_by('nama','RANDOM')
+        ->get('fandom')
+        ->result_array();
     }
 }
 
