209ce6b6d5e9a361431e910733163db29062718c
rifkacanalisa <50352632+rifkacanalisa@users.noreply.github.com>
2021-02-18 11:37:28 +0700

    solving nambah pilihan card gambar fandom

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -11,18 +11,27 @@
 <div class="container">
     <h3>Fandom</h3>
     <?php foreach ($homes as $h) : ?>
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="<?= $h['pictures']; ?>" width="150" height="200">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="<?= $h['pictures']; ?>" width="150" height="250">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $h['nama']; ?></h5>
+                        <p class="card-text"><?= $h['deskripsi']; ?></p>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-8">
+        </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="<?= $h['pictures']; ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"><?= $h['nama']; ?></h5>
                     <p class="card-text"><?= $h['deskripsi']; ?></p>
                 </div>
             </div>
+        <?php endforeach; ?>
+
         </div>
-    </div>
-    <?php endforeach; ?>
-</div>
