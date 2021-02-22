e50e7ede6c1a0378dc4e9f37aedfb7182601cb24
rifkacanalisa <50352632+rifkacanalisa@users.noreply.github.com>
2021-02-22 08:30:36 +0700

    change sort add class button index post

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -31,7 +31,7 @@
             </div>
             <div class="form-group col-md-4">
                 <label for=""></label>
-                <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
+                <button type="submit" class="btn btn-primary form-control" name="simpan">Simpan</button>
             </div>
         </div>
     </form>
